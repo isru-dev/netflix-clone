1bdd7f37d2cf0534db92a44d230448ecf6052250
israel Gezahegn <isrubest18@gmail.com>
2026-04-01 09:09:14 +0300

    crate header

--- netflix/netflix-web/src/App.jsx
+++ netflix/netflix-web/src/App.jsx
@@ -1,12 +1,12 @@
 //import { useState } from 'react'
 import './App.css'
-
+import {Home} from './pages/home'
 function App() {
   
 
   return (
     <>
-     
+     <Home />
     </>
   )
 }
---
+++ netflix/netflix-web/src/components/header.jsx
@@ -0,0 +1,36 @@
+import './header.css';
+import logo from '../assets/netlogo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+export function Header() {
+  return (
+    <div className="header">
+      <div className='header-container'>
+        <div className='left-side'>
+          <ul>
+            <img src={logo} alt="netflix logo" className='img' />
+            <li>Home</li>
+            <li>TvShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse By Languages</li>
+          </ul>
+
+        </div>
+        <div className='header-right'>
+          <ul>
+            <li><SearchIcon /></li>
+            <li><NotificationsNoneIcon /></li>
+            <li><AccountBoxIcon /></li>
+            <li><ArrowDropDownIcon /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  );
+}
