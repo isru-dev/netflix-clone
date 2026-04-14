906d49dd9d882533fcd737dd35eddeb932c08cf8
israel Gezahegn <isrubest18@gmail.com>
2026-04-15 02:16:10 +0300

    finshing netflix banner

--- netflix/netflix-web/src/App.jsx
+++ netflix/netflix-web/src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 import {Home} from './pages/home'
 function App() {
-  
 
   return (
     <>
--- netflix/netflix-web/src/pages/home.jsx
+++ netflix/netflix-web/src/pages/home.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/header";
 import { Fotter } from '../components/fotter'
+import { Banner } from "../components/banner";
 export function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Fotter />
     </>
   );
